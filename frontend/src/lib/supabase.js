dec11112c0da1a7521c3ba1fce4e180d9b8e7996
Asilbek Ilhomov <ilhomovasilbek7@gmail.com>
2026-04-15 03:28:03 +0500

    fix: supabase connected

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
-);
+const supabaseUrl = "https://uqnmvanmucgvngvbgjqa.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxbm12YW5tdWNndm5ndmJnanFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwOTM1ODQsImV4cCI6MjA5MTY2OTU4NH0.uKS8sBJ3TrWLcad5KsR_WyVJOp_JPTw3hMb8WG4TXzA";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
